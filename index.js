41d5a9272642678e90ba85c03095c2247017325c
AAYUSH-SAHAY-24326711019 <aayush.mcastudent24.1062@cimage.in>
2025-08-24 13:17:50 +0530

    Taking user input by form and print by js function

--- index.js
+++ index.js
@@ -15,11 +15,20 @@ let isStudent = true;
 
 document.getElementById('new-para3').innerHTML=`<b>Name</b> = ${fullName}<br><b>Age</b> = ${age}<br><b>Studying</b> = ${isStudent}`;
 
-let userName = window.prompt("Tell us your user name :");
-let userAge = window.prompt("Tell us your age :");
-let whatIsUser = window.prompt("Tell us whether you are a professional / student :")
+// let userName = window.prompt("Tell us your user name :");
+// let userAge = window.prompt("Tell us your age :");
+// let whatIsUser = window.prompt("Tell us whether you are a professional / student :")
 
-document.getElementById('new-para4').innerHTML=`<h4>Visitor Section</h4><b>Name</b> = ${userName}<br><b>Age</b> = ${userAge}<br><b>Role :</b> = ${whatIsUser}<br>`;
+// document.getElementById('new-para4').innerHTML=`<h4>Visitor Section</h4><b>Name</b> = ${userName}<br><b>Age</b> = ${userAge}<br><b>Role :</b> = ${whatIsUser}<br>`;
+
+
+
+document.getElementById("submit1").onclick=function(){
+    let userName = document.getElementById("inp1").value;
+    let userAge = document.getElementById("inp2").value;
+    let whatIsUser = document.getElementById("inp3").value;
+    document.getElementById('new-para4').innerHTML=`<h4>Visitor Section</h4><b>Name</b> = ${userName}<br><b>Age</b> = ${userAge}<br><b>Role :</b> = ${whatIsUser}<br>`;
+}
 
 
 
