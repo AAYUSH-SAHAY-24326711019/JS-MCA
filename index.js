4377ce409ad49d8111d736fa5a2e10d1e65d4762
AAYUSH-SAHAY-24326711019 <aayush.mcastudent24.1062@cimage.in>
2025-08-24 12:47:59 +0530

    To show type of value show in string

--- index.js
+++ index.js
@@ -4,4 +4,7 @@ document.getElementById('mPara').innerText="This is my paragraph";
 window.alert("Welcome to our page !")
 
 let noOfDays = 7;
+
 document.getElementById('new-para1').innerText=`The no. of Days in a week is = ${noOfDays}`;
+
+document.getElementById('new-para2').innerText=`The type of noOfDays variable is = ${typeof noOfDays}`;
