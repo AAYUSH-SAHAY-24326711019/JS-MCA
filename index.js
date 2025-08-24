1c244ee150950eb15cb03154689df47673bb1c86
AAYUSH-SAHAY-24326711019 <aayush.mcastudent24.1062@cimage.in>
2025-08-24 12:54:40 +0530

    To show the use of innerHTML

--- index.js
+++ index.js
@@ -8,3 +8,9 @@ let noOfDays = 7;
 document.getElementById('new-para1').innerText=`The no. of Days in a week is = ${noOfDays}`;
 
 document.getElementById('new-para2').innerText=`The type of noOfDays variable is = ${typeof noOfDays}`;
+
+let fullName = "Aayush Sahay";
+let age = 21;
+let isStudent = true;
+
+document.getElementById('new-para3').innerHTML=`<b>Name</b> = ${fullName}<br><b>Age</b> = ${age}<br><b>Studying</b> = ${isStudent}`;
