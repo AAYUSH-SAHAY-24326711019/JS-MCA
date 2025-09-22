8ab3bbb332d8bc8142fc3ff982d66a8fd05cb68a
AAYUSH-SAHAY-24326711019 <aayush.mcastudent24.1062@cimage.in>
2025-09-22 07:30:35 +0530

    Setup of node js and js language learning

---
+++ video1/video1.js
@@ -0,0 +1,8 @@
+console.log(10);
+console.log("Aayush");
+//This is the code of the java cript
+//This is the code run by the node js
+//so that we can debig the code on our own system
+
+// alert("Hello, 1st file on node js");
+//will work in a browser and not in the system
