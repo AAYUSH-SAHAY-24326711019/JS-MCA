07aea4541c6de4ab0e08975f384413462768aa9b
AAYUSH-SAHAY-24326711019 <aayush.mcastudent24.1062@cimage.in>
2025-09-22 07:55:33 +0530

    Variable types in JavaScript (Rules)

---
+++ video3/variables.js
@@ -0,0 +1,20 @@
+a=45;
+console.log(a);
+
+courseName="javascript";
+courseId="123";
+courseDept="Computer-Science";
+
+console.log(courseDept,courseId,courseName);
+
+console.table([courseDept,courseId,courseName]);
+
+//const : make it readonly variable
+
+// let : it is basis to scope to a function.
+// we cannot redefine the let variable in the 
+// same block;
+
+// var we can redefine it multiple times.
+
+//1st letter can be _ , $ and not any keyword.
